f6117fdbd0796c1448032db04430fb79e85f523a
jasmine <jasmine@alta3.com>
2020-12-08 22:58:59 +0000

    list and Dictionaire challenge

---
+++ challenge/challengelistdic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Create a list of dog names (at least 3)
+list1 = ["ace","jake","snow"]
+print(list1)
+
+# Create another list of cat names (at least 3)
+list2 = ["lemon","pepper","sparky"]
+print(list2)
+# Append the cats list  onto the dogs list (so it is a single list)
+list1.append(list2)
+
+# Print out the first dog name from your single list
+print(list1)
+
+# Print out the second cat name from your single list
+print(list2)
+
+# Create a dictionary with the keys "first_dog" and "second_cat".
+# Use the appropriate values from your list as the values for the dictionary keys
+# Print out the dictionary
+# Print out the value of "first_dog" and "second_cat" from the dictionary
