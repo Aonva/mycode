5cbd72cb6ba807217f9c32dd8ab7c19cbf864067
jasmine <jasmine@alta3.com>
2020-12-09 00:17:50 +0000

    join and split

--- challenge/challengelistdic.py
+++ challenge/challengelistdic.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 
 # Create a list of dog names (at least 3)
-list1 = ["ace","jake","snow"]
-print(list1)
+dogs = ["ace","jake","snow"]
+print(dogs)
 
 # Create another list of cat names (at least 3)
-list2 = ["lemon","pepper","sparky"]
-print(list2)
+cats = ["lemon","pepper","sparky"]
+print(cats)
 # Append the cats list  onto the dogs list (so it is a single list)
-list1.append(list2)
-
+cats.append(dogs)
 # Print out the first dog name from your single list
-print(list1)
+print(dogs[0])
 
-# Print out the second cat name from your single list
-print(list2)
+# Print out the second cat name from your single list)
+print(cats[1])
 
 # Create a dictionary with the keys "first_dog" and "second_cat".
+
 # Use the appropriate values from your list as the values for the dictionary keys
+catdog= {"first_dog":  dogs
 # Print out the dictionary
 # Print out the value of "first_dog" and "second_cat" from the dictionary
