9c41f4c28dd53acaaf22d0692b5ba36c6b439f1d
jasmine <jasmine@alta3.com>
2020-12-11 22:29:29 +0000

    Playing with python pip packages

---
+++ color/color3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Alta3 Research || Author RZFeeser@alta3.com
+Learning how to use functions"""
+
+## Installs the crayons package.
+## python3 -m pip install crayons
+## import statements ALWAYS go up top
+import crayons
+
+def main():
+    """Runtime code. always indent a function"""
+    print(crayons.red('Jasmine red'))
+
+    # print 'white string' in white
+    print(crayons.white('Jasmine white'))
+main()
